bc98160199fc114906d25aec9180b8f9927806c1
denisamihut <denisa.m_19@yahoo.com>
2022-10-30 10:36:27 +0200

    added new icons for CRUD functionalities

--- javascript.js
+++ javascript.js
@@ -1,17 +1,15 @@
-$("input[type='text']").keypress(function (e) {
-    if (e.which === 13) {
-        var todoText = $(this).val();
-        if ($(this).val() !== "") {
-            $("#list").append("<li>" + todoText + "</li>");
-        }
-        $(this).val("");
-    }
-});
-// $(".add").click(function () {
-//     $("input[type='text']").fadeToggle(200);
-// });
-$(document).ready(function(){
+$(document).ready(function () {
     $('#inp').hide();
-    $('#add').click(function() {
+    $('#add').click(function () {
         $('#inp').show();
-    });});
+    });
+    $("input[type='text']").keypress(function (e) {
+        if (e.which === 13) {
+            var todoText = $(this).val();
+            if ($(this).val() !== "") {
+                $("#list").append("<li>" + todoText + "</li>");
+            }
+            $(this).val("");
+        }
+    });
+});
