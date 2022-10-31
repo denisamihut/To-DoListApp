ee331100515579e1804582144f111df6cbc9c336
denisamihut <denisa.m_19@yahoo.com>
2022-10-31 20:09:30 +0200

    finished CRUD on all 4 tables

--- javascript.js
+++ javascript.js
@@ -12,46 +12,45 @@ $(document).ready(function () {
         var type2 = "School Task";
         var type3 = "Household Task";
 
-
-
         $('#todo_list').append('<li class="pb-1 h5"><span>' + task_name + " " + task_duration + " " + task_due_date + " " + task_type + ' </span> <button type="button" class="btn btn-primary" id="edit' + counter + '"> <i class="fas fa-pencil"></i> Edit</button>' + " " + '<button type="button" class="done btn btn-danger" id="delete' + counter + '"> <i class="fas fa-trash"></i> Delete</button>' + '</li>');
-       
-       
-        if (task_type == type1){
-            $('#todo_work').append('<li class="pb-1 h5"><span>' + task_name + " " + task_duration + " " + task_due_date + " " + task_type + ' </span> <button type="button" class="btn btn-primary" id="editw' + counter + '"> <i class="fas fa-pencil"></i> Edit</button>' + " " + '<button type="button" class="done btn btn-danger" id="deletew' + counter + '"> <i class="fas fa-trash"></i> Delete</button>' + '</li>');    
+        if (task_type == type1) {
+            $('#todo_work').append('<li class="pb-1 h5"><span>' + task_name + " " + task_duration + " " + task_due_date + ' </span> <button type="button" class="btn btn-primary" id="editw' + counter + '"> <i class="fas fa-pencil"></i> Edit</button>' + " " + '<button type="button" class="done btn btn-danger" id="deletew' + counter + '"> <i class="fas fa-trash"></i> Delete</button>' + '</li>');
         }
-        if (task_type == type2){
-            $('#todo_home').append('<li class="pb-1 h5"><span>' + task_name + " " + task_duration + " " + task_due_date + " " + task_type + ' </span> <button type="button" class="btn btn-primary" id="edith' + counter + '"> <i class="fas fa-pencil"></i> Edit</button>' + " " + '<button type="button" class="done btn btn-danger" id="deleteh' + counter + '"> <i class="fas fa-trash"></i> Delete</button>' + '</li>');    
+        if (task_type == type2) {
+            $('#todo_home').append('<li class="pb-1 h5"><span>' + task_name + " " + task_duration + " " + task_due_date + ' </span> <button type="button" class="btn btn-primary" id="edith' + counter + '"> <i class="fas fa-pencil"></i> Edit</button>' + " " + '<button type="button" class="done btn btn-danger" id="deleteh' + counter + '"> <i class="fas fa-trash"></i> Delete</button>' + '</li>');
         }
-        if (task_type == type3){
-            $('#todo_school').append('<li class="pb-1 h5"><span>' + task_name + " " + task_duration + " " + task_due_date + " " + task_type + ' </span> <button type="button" class="btn btn-primary" id="edits' + counter + '"> <i class="fas fa-pencil"></i> Edit</button>' + " " + '<button type="button" class="done btn btn-danger" id="deletes' + counter + '"> <i class="fas fa-trash"></i> Delete</button>' + '</li>');    
+        if (task_type == type3) {
+            $('#todo_school').append('<li class="pb-1 h5"><span>' + task_name + " " + task_duration + " " + task_due_date + ' </span> <button type="button" class="btn btn-primary" id="edits' + counter + '"> <i class="fas fa-pencil"></i> Edit</button>' + " " + '<button type="button" class="done btn btn-danger" id="deletes' + counter + '"> <i class="fas fa-trash"></i> Delete</button>' + '</li>');
         }
-
 
         $('#edit' + counter).click(function () {
-            $(this).prev().attr('contenteditable', 'true');
-            $(this).prev().focus();
-        });
-        $('#editw' + counter).click(function () {
-            $(this).prev().attr('contenteditable', 'true');
-            $(this).prev().focus();
-        });
-        $('#edith' + counter).click(function () {
-            $(this).prev().attr('contenteditable', 'true');
-            $(this).prev().focus();
-        });
-        $('#edits' + counter).click(function () {
             $(this).prev().attr('contenteditable', 'true');
             $(this).prev().focus();
         });
         $('#delete' + counter).click(function () {
             $(this).parent().remove();
         });
+
+        $('#editw' + counter).click(function () {
+            $(this).prev().attr('contenteditable', 'true');
+            $(this).prev().focus();
+        });
         $('#deletew' + counter).click(function () {
             $(this).parent().remove();
         });
+
+        $('#edith' + counter).click(function () {
+            $(this).prev().attr('contenteditable', 'true');
+            $(this).prev().focus();
+        });
         $('#deleteh' + counter).click(function () {
             $(this).parent().remove();
+        });
+
+
+        $('#edits' + counter).click(function () {
+            $(this).prev().attr('contenteditable', 'true');
+            $(this).prev().focus();
         });
         $('#deletes' + counter).click(function () {
             $(this).parent().remove();
@@ -61,4 +60,6 @@ $(document).ready(function () {
     $(function () {
         $('#add').on('click', enter_task);
     });
+
+
 });
